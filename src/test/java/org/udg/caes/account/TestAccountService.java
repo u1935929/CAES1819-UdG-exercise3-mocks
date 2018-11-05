acd9373e862f3bf5c6162a2720e228612df38004
magalenyo <magalenyo7@gmail.com>
2018-11-05 18:18:57 +0100

    Verifications:  - Update Account acc1 realitzat 1 vegada  - Update Account acc2 realitzat 1 vegada  - Acc2 Credit realitzat 1 vegada  - Acc1 Debit realitzat 1 vegada  - Assert equals de la acc1 realitzat correctament  - Assert equals de la acc2 realitzat correctament

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -1,20 +1,38 @@
 package org.udg.caes.account;
 
-import mockit.Expectations;
-import mockit.Verifications;
+import mockit.*;
 import org.junit.jupiter.api.Test;
-
-import mockit.Tested;
-import mockit.Mocked;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAccountService {
 
+  @Injectable Account acc1;
+  @Injectable Account acc2;
+
 
   @Test
   void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+    as = new AccountService();
+    as.setAccountManager(am);
+    acc1 = new Account("Miguel",100);
+    acc2 = new Account("Angel",350);
+
+    new Expectations(){{
+      am.findAccount("Miguel"); result = acc1;
+      am.findAccount("Angel"); result = acc2;
+    }};
+
+    as.transfer("Miguel","Angel",50);
+
+    new Verifications(){{
+      am.updateAccount(acc1); times = 1;
+      am.updateAccount(acc2); times = 1;
+      acc2.credit(0); times = 1;
+      acc1.debit(0); times = 1;
+      assertEquals(acc1.getBalance(), 50);
+      assertEquals(acc2.getBalance(),400);
+    }};
   }
 }
